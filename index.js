b8c138c0ca18517231582d705040ab68b472b80d
willy0483 <willyjensen251@gmail.com>
2024-11-12 11:15:50 +0100

    express

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 import express, { request } from "express";
 const app = express();
-const port = 3232;
+
+import dotenv from "dotenv";
+dotenv.config();
+console.log(process.env);
+
+const port = process.env.PORT;
+const api_key = process.env.supabase_Key;
+const api_Url = process.env.supabase_Url;
+
+console.log(api_key, api_Url);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -14,10 +23,16 @@ app.get("/contact", (req, res) => {
   res.send("Contact");
 });
 
+app.get("/produkt", (req, res) => {
+  res.send("produkt");
+});
+
 app.listen(port, () => {
   console.log(`
     express køre på port ${port}   
     http://localhost:${port}/
     http://localhost:${port}/about
-    http://localhost:${port}/contact`);
+    http://localhost:${port}/contact
+    http://localhost:${port}/produkt
+    `);
 });
