9de9c447a762121ab10ad08ef5f569a8c53b1410
willy0483 <willyjensen251@gmail.com>
2024-11-18 12:12:25 +0100

    ok

--- controllers/songController.js
+++ controllers/songController.js
@@ -3,10 +3,10 @@ import { songModel } from "../models/songModel.js";
 
 export const songController = express.Router();
 
-// songController.get("/songs", async (req, res) => {
-//   let data = await songModel.getAllRecords();
-//   console.log(data);
-// });
+songController.get("/songs", async (req, res) => {
+  let data = await songModel.getAllRecords();
+  console.log(data);
+});
 
 songController.get("/songs/:id([0-9]*)", async (req, res) => {
   const data = await songModel.getRecordsById(req.params.id);
